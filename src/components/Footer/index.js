7159510e778deb52e6b5aef07ed3272f6d1c5497
Caique <caique_giovannini@hotmail.com>
2020-07-29 17:01:02 -0300

    altera footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,16 +4,29 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://www.linkedin.com/in/caique-giovannini/" target="_blank">
+        <img src="https://raw.githubusercontent.com/caiquegiovannini/imersao-css/master/img/titulo.png" alt="caique giovannini logo"/>
       </a>
       <p>
         Orgulhosamente criado durante a
         {' '}
-        <a href="https://www.alura.com.br/">
+        <a href="https://www.alura.com.br/" target="_blank">
           Imersão React da Alura
         </a>
       </p>
+      
+      <ul>
+        <li>
+          <a href="https://github.com/caiquegiovannini" target="_blank">
+            <img src="https://github.com/caiquegiovannini/weather-app/blob/master/images/contact_icons/github_white.png?raw=true" alt="github icon" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/caique-giovannini/" target="_blank">
+            <img src="https://github.com/caiquegiovannini/weather-app/blob/master/images/contact_icons/linkedin.png?raw=true" alt="linkedin icon" />
+          </a>
+        </li>
+      </ul>
     </FooterBase>
   );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -10,6 +10,27 @@ export const FooterBase = styled.footer`
   padding-bottom: 32px;
   color: var(--white);
   text-align: center;
+
+  img {
+    width: 25%;
+  }
+
+  ul {
+    display: flex;
+    justify-content: center;
+    margin-top: 35px;
+
+    li {
+      list-style: none;
+      &:nth-child(2) {
+        margin-left: 50px;
+      }
+
+      img {
+        width: 35px;
+      }
+    }
+  }
   @media (max-width: 800px) {
     margin-bottom: 50px;
   }
