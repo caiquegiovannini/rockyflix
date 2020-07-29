a460fa73384e3740786d01fa124c01e68f6f0d2a
Caique <caique_giovannini@hotmail.com>
2020-07-29 17:04:03 -0300

    arruma links do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,25 +4,25 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.linkedin.com/in/caique-giovannini/" target="_blank">
+      <a href="https://www.linkedin.com/in/caique-giovannini/">
         <img src="https://raw.githubusercontent.com/caiquegiovannini/imersao-css/master/img/titulo.png" alt="caique giovannini logo"/>
       </a>
       <p>
         Orgulhosamente criado durante a
         {' '}
-        <a href="https://www.alura.com.br/" target="_blank">
+        <a href="https://www.alura.com.br/">
           Imersão React da Alura
         </a>
       </p>
       
       <ul>
         <li>
-          <a href="https://github.com/caiquegiovannini" target="_blank">
+          <a href="https://github.com/caiquegiovannini">
             <img src="https://github.com/caiquegiovannini/weather-app/blob/master/images/contact_icons/github_white.png?raw=true" alt="github icon" />
           </a>
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/caique-giovannini/" target="_blank">
+          <a href="https://www.linkedin.com/in/caique-giovannini/">
             <img src="https://github.com/caiquegiovannini/weather-app/blob/master/images/contact_icons/linkedin.png?raw=true" alt="linkedin icon" />
           </a>
         </li>
