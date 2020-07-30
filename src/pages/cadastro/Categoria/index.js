3c9081299dc7c3d0ade79291109f6baa4a2d5791
Caique <caique_giovannini@hotmail.com>
2020-07-30 18:10:31 -0300

    adiciona server-json para salvar os dados

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
     descricao: '',
     cor: '#000',
-  }
+  };
 
   const [valores, setValores] = useState(valoresIniciais);
   const [categorias, setCategorias] = useState([]);
@@ -35,12 +36,25 @@ function CadastroCategoria() {
     setValores(valoresIniciais);
   }
 
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias';
+
+    fetch(URL)
+      .then(async (res) => {
+        const data = await res.json();
+
+        setCategorias([
+          ...data,
+        ]);
+      });
+  }, []);
+
   return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
 
       <form onSubmit={handleSubmit}>
-        <FormField 
+        <FormField
           label="Nome da Categoria"
           type="text"
           name="nome"
@@ -48,33 +62,30 @@ function CadastroCategoria() {
           onChange={handleChange}
         />
 
-        <div>
-          <label>
-            Descrição da Categoria:
-            <textarea
-              value={valores.descricao}
-              name="descricao"
-              onChange={handleChange}
-            />
-          </label>
-        </div>
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={valores.descricao}
+          onChange={handleChange}
+        />
 
         <FormField
-          label="Cor da Categoria"
+          label="Cor"
           type="color"
           name="cor"
           value={valores.cor}
           onChange={handleChange}
         />
 
-        <button>
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
 
       <ul>
-        {categorias.map((categoria, index) => (
-          <li key={`${categoria.nome}-${index}`}>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>
             {categoria.nome}
           </li>
         ))}
